4712362bc2efab5f5f2da757acf7a7de29ffc337
Michael Schlueter <michael@johalla.de>
2023-04-11 23:11:24 +0200

    Implement php7.4+ preferred serialization.

--- lib/Horde/ActiveSync/Folder/Collection.php
+++ lib/Horde/ActiveSync/Folder/Collection.php
@@ -63,6 +63,24 @@ class Horde_ActiveSync_Folder_Collection extends Horde_ActiveSync_Folder_Base im
     }
 
     /**
+     * Serialize this object. PHP 8+
+     *
+     * @return array  The data to serialize as an array.
+     */
+    public function __serialize(): array 
+    {
+        return
+        [
+            's' => $this->_status,
+            'f' => $this->_serverid,
+            'c' => $this->_class,
+            'lsd' => $this->_lastSinceDate,
+            'sd' => $this->_softDelete,
+            'i' => $this->haveInitialSync,
+            'v' => self::VERSION
+        ];
+    }
+    /**
      * Reconstruct the object from serialized data.
      *
      * @param string $data  The serialized data.
@@ -71,6 +89,25 @@ class Horde_ActiveSync_Folder_Collection extends Horde_ActiveSync_Folder_Base im
     public function unserialize($data)
     {
        $data = @json_decode($data, true);
+        if (!is_array($data) || empty($data['v']) || $data['v'] != self::VERSION) {
+            throw new Horde_ActiveSync_Exception_StaleState('Cache version change');
+        }
+        $this->_status = $data['s'];
+        $this->_serverid = $data['f'];
+        $this->_class = $data['c'];
+        $this->haveInitialSync = $data['i'];
+        $this->_lastSinceDate = empty($data['lsd']) ? 0 : $data['lsd'];
+        $this->_softDelete = empty($data['sd']) ? 0 : $data['sd'];
+    }
+
+    /**
+     * Reconstruct the object from serialized data. PHP 8+
+     * 
+     * @param array $data  The serialized data.
+     * @throws Horde_ActiveSync_Exception_StaleState
+     */
+    public function __unserialize(array $data): void
+    {
         if (!is_array($data) || empty($data['v']) || $data['v'] != self::VERSION) {
             throw new Horde_ActiveSync_Exception_StaleState('Cache version change');
         }
